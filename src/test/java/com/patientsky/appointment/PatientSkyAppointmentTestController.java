f10b72e75aaa85905fc545b6fa3fe630920eb9ce
deepthi-7 <72294385+deepthi-7@users.noreply.github.com>
2024-05-12 13:40:48 +0530

    Add files via upload

---
+++ src/test/java/com/patientsky/appointment/PatientSkyAppointmentTestController.java
@@ -0,0 +1,32 @@
+package com.patientsky.appointment;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.patientsky.appointment.controller.AppointmentController;
+import com.patientsky.appointment.model.CalendarsData;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("unittest")
+public class PatientSkyAppointmentTestController {
+
+	private String directory = "src/test/resources";
+
+	@InjectMocks
+	AppointmentController appointmentController;
+
+	@Test
+	public void getPatientDetailsTest() {
+		
+		ReflectionTestUtils.setField(appointmentController, "patientDataDir", directory);
+		List<CalendarsData> calendarsData = ReflectionTestUtils.invokeMethod(appointmentController, "getPatientDetails");
+		Assertions.assertEquals(1,calendarsData.size());
+	}
+}
